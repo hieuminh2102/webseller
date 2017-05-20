eaf8cec240bf4fef444a320e9b5c052a5e2b3d9c
xuanhong95 <mrh95hn@gmail.com>
2017-05-20 22:28:22 +0700

    Fix footer alway bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,11 +35,14 @@
         .dropdown-menu li a{
             color: white;
         }
-    </style>
+        html{
+            height: 100%;
+        }
+  </style>
 </head>
-<body id="app-layout">
+<body id="app-layout" style="height: 100%">
     <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
+        <div class="container-fluid">
             <div class="navbar-header">
 
                 <!-- Collapsed Hamburger -->
@@ -120,13 +123,14 @@
         </div>
     </nav>
 
-    @yield('content')
+    <div class="col-md-12" style="min-height:100%; margin-top:50px;">
+        @yield('content')
+    </div>
 
     <div class="col-md-12" style="clear:both;">
         <!--footer start from here-->
-        <link href="/css/footer.css" rel="stylesheet">
         <style >
-            .copyright { min-height:40px; background-color:#000000;}
+            .copyright { min-height:40px; background-color:#42b2b8;}
             .copyright p { text-align:left; color:#FFF; padding:10px 0; margin-bottom:0px;}
             .footer {
                 margin-top: 30px;
@@ -138,38 +142,12 @@
             }
         </style>
         <div class="footer">
-            <footer style="background-image:url('/images/footersievn.png'); background-repeat:repeat-x;">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 footerleft ">
-
-                            <p><i class="fa fa-map-pin"></i>Address: Room 201, D7 Building, HUST | No.1, Dai Co Viet Street, Hanoi, Vietnam.</p>
-                            <p><i class="fa fa-phone"></i>Tel:(+84)04.3868.3407 & 3868.2261 | Fax:(+84)04.3868.3409</p>
-                            <p><i class="fa fa-envelope"></i>Email: info@sie.edu.vn | Website: http://sie.hust.edu.vn</p>
-
-                        </div>
-
-                        <div class="col-md-3 col-sm-3 paddingtop-bottom " >
-                            <div class="fb-page" data-href="https://www.facebook.com/sie.hust.edu.vn/" data-tabs="timeline" data-height="300" data-small-header="false" style="margin-bottom:15px;" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                                <div class="fb-xfbml-parse-ignore">
-                                    <blockquote cite="https://www.facebook.com/sie.hust.edu.vn/"><a href="https://www.facebook.com/sie.hust.edu.vn/">Facebook</a></blockquote>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-3 col-sm-3 paddingtop-bottom">
-                            <div class="logofooter" > <img src="/images/logonho.png" alt="sie_logo"></div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <!--footer start from here-->
-
             <div class="copyright" style="height:50px">
                 <div class="container">
                     <div class="col-md-6">
                         <p>Copyright© School of International Education | HUST</p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-offset-3 col-md-3">
                         <p>Powered by: Students</p>
                     </div>
                 </div>
